abeb5638494d651861c3ea574122630dc8a73d83
Wiek86 <adrian.witek12@gmail.com>
2025-05-29 22:13:22 +0200

    Update MainWindow.cpp
    dodanie komunikatów zapisanie kodu i plik otwarty

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,6 +1,7 @@
 #include "MainWindow.h"
 #include <QVBoxLayout>
 #include <QWidget>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent) {
@@ -16,6 +17,36 @@ MainWindow::MainWindow(QWidget *parent)
 
     resize(800, 600);
     move(100, 100);
+}
+
+void MainWindow::openFile() {
+    QString filename = QFileDialog::getOpenFileName(this, "Otwórz plik");
+    if (!filename.isEmpty()) {
+        QFile file(filename);
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            QTextStream in(&file);
+            ui->textEdit->setPlainText(in.readAll());
+            file.close();
+            QMessageBox::information(this, "Otworzono", "Plik otwarty.");
+        } else {
+            QMessageBox::warning(this, "Błąd", "Nie można otworzyć pliku.");
+        }
+    }
+}
+
+void MainWindow::saveFile() {
+    QString filename = QFileDialog::getSaveFileName(this, "Zapisz plik");
+    if (!filename.isEmpty()) {
+        QFile file(filename);
+        if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+            QTextStream out(&file);
+            out << ui->textEdit->toPlainText();
+            file.close();
+            QMessageBox::information(this, "Zapisano", "Plik zapisany.");
+        } else {
+            QMessageBox::warning(this, "Błąd", "Nie można zapisać pliku.");
+        }
+    }
 }
 
 MainWindow::~MainWindow() = default;
